8bb5cccbd38642a0f3b98b68997ef102509625bc
Peak_Kap <132248084+CJManiscalco@users.noreply.github.com>
2025-02-11 10:08:29 -0500

    Create Comments.java

---
+++ Comments.java
@@ -0,0 +1,22 @@
+// CJ Maniscalco
+// wow guys look! I can add a comment! Hello!!
+/* This is the second one of me using the comments! Wow!!
+test as well!
+*/ 
+/************************************
+*Project: Demo for Comments
+*Programmer: Mr. Data
+*Date: June, 2003
+*Program Name: Comments.java
+*************************************/
+public class Comments
+{
+public static void main (String[ ] args)
+{
+System.out.println("Hi"); //message to user
+System.out.println("LabOneA"); //assignment
+}
+}
+/* if you want to write a detailed comment, and it wraps
+around the screen, use this block style of commenting.
+*/
